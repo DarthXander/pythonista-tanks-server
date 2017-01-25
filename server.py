c1f3ce13f0a34463e638d3bb72458750f873fc25
Xander Clair <xanthochroidxander@gmail.com>
2017-01-24 19:43:38 -0800

    fixing bugs

--- server.py
+++ server.py
@@ -5,19 +5,20 @@ import sys
 # coroutine for echo server
 async def echo(websocket, path):
 	print("connection with {!r}".format(path))
-    data = await websocket.recv() # recieve some bytes
-    print("received {!s} data: {!r}".format(len(data), data))
-
-    # send them back!
-    await websocket.send(data)
-    print("sent back")
+	data = await websocket.recv() # recieve some bytes
+	print("received {!s} data: {!r}".format(len(data), data))
+	
+	await websocket.send(data) # send them back!
+	print("sent back")
 
 # get port environment variable from command line arguments
 args = sys.argv
 port = args[1]
 
+address = ('0.0.0.0', port)
+
 # start the async server
-server = websockets.serve(echo, '0.0.0.0', port)
+server = websockets.serve(echo, address[0], address[1])
 
 # get event loop
 loop = asyncio.get_event_loop()
