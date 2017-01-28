951f4895daaab57bc52aa665fb4c62bd02167db1
Xander Clair <xanthochroidxander@gmail.com>
2017-01-27 16:12:07 -0800

    changes

--- server.py
+++ server.py
@@ -6,23 +6,42 @@ import sys
 # | connection id | type     | data
 # | (2 bytes)     | one byte | ....
 
+class PlayerInfo (object):
+	def __init__(self, **kwargs):
+		for name, arg in kwargs:
+			setattr(self, name, arg)
+
+new_connection = 0x0
+get_tank_positions = 0x1
+send_tank_position = 0x2
+send_nickname = 0x3
+get_nicknames = 0x4
+
 connections = []
-data = {}
+players = {}
 
 # coroutine for echo server
 async def echo(websocket, path):
 	print("connection with {!r}".format(websocket.remote_address))
 	data = await websocket.recv() # recieve some bytes
 	print("received {!s} data: {!r}".format(len(data), data))
+	if len(data) < 3:
+		raise AssertionError()
 	idnum = data[0:2]
 	messagetype = data[2]
-	if route:
-		route_to = data[2:4]
+	message = data[3:]
+	if messagetype == new_connection:
+		print("establishing new connection with {!s}, sending 0x1 to confirm".format(idnum))
+		await websocket.send(bytes([0x1]))
+		connections.append(idnum)
+		print("confirmed. {!s} connections total".format(len(connections)))
+	elif messagetype == send_nickname:
+		players[idnum] = PlayerInfo(nickname = message.decode("utf-8"))
+	elif messagetype = send_tank_position:
+		if idnum in players:
+			players[idnum].position = ()
 
-
-
-	await websocket.send(data) # send them back!
-	print("sent back")
+	
 
 # get port environment variable from command line arguments
 args = sys.argv
